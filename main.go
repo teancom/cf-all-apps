f4967ccd7fb2ac6f99dbb11389669fcc0b7a1776
David Bishop <dbisj@allstate.com>
2019-01-17 07:38:52 -0700

    Add ability to use seperate config file, rework error handling

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/cloudfoundry-community/go-cfclient"
@@ -24,12 +26,11 @@ type Org map[string]Space
 type Space []string
 
 func main() {
-	// Vendor dependencies
 	// Add unit testing
 	// Add bash/zsh/fish completions
 	// Add a homebrew tap to install it
 	// Add a pre-compiled version of OSX, linux, and Windows(?)
-	// Add ability to use config file vs. "native" CF
+	// Add ability to use config file vs. "native" CF auth
 
 	usage := `cf-all-apps - List all apps in your targeted CF
 
@@ -43,6 +44,7 @@ Options:
     -j, --json              Output in JSON instead of YAML
     -k, --skip-ssl-verify   Disable cryptographic verification of the CF API
     -r, --running           Only show running apps
+    -c, --config            Use a config file for auth instead of relying on the cf cli
 `
 	arguments, err := docopt.ParseDoc(usage)
 	handleErr("Unable to parse arguments.", err)
@@ -51,16 +53,27 @@ Options:
 	jsonOut, _ := arguments.Bool("--json")
 	skip, _ := arguments.Bool("--skip-ssl-verify")
 	running, _ := arguments.Bool("--running")
+	authConfig, _ := arguments.String("--config")
 
 	if version {
 		fmt.Println("0.0.1")
 		os.Exit(0)
 	}
 
-	configFile, err := os.Open(fmt.Sprintf("%s/.cf/config.json", os.Getenv("HOME")))
-	handleErr("Unable to read the CF config file. Does it exist? You may have to log in via the cf cli.", err)
+	configFilePath := ""
+	if authConfig == "" {
+		configFilePath = fmt.Sprintf("%s/.cf/config.json", os.Getenv("HOME"))
+	} else {
+		configFilePath = authConfig
+	}
 
-	cfConfig := readCfConfig(configFile)
+	configFile, err := os.Open(configFilePath)
+	handleErr("Unable to read the specified config file.", err)
+	cfConfig, err := readCfConfig(configFile)
+	handleErr("Unable to parse your cloudfoundry config file. Try deleting it and re-logging in via the cf cli.", err)
+
+	err = validateCfConfig(cfConfig)
+	handleErr("Unable to validate your cloudfoundry config", err)
 
 	c := &cfclient.Config{
 		ApiAddress:        cfConfig.Api,
@@ -74,13 +87,13 @@ Options:
 	allApps, err := client.ListApps()
 	handleErr("Unable to list the apps", err)
 
-	outputApps(appsInFoundation(allApps, running), jsonOut)
+	fmt.Println(outputApps(appsInFoundation(allApps, running), jsonOut))
 
 }
 
 func handleErr(message string, err error) {
 	if err != nil {
-		fmt.Sprintf("%s: %s\n", message, err)
+		fmt.Printf("%s: %s\n", message, err)
 		os.Exit(1)
 	}
 }
@@ -91,6 +104,7 @@ func appsInFoundation(apps []cfclient.App, running bool) Foundation {
 		orgName := app.SpaceData.Entity.OrgData.Entity.Name
 		spaceName := app.SpaceData.Entity.Name
 		appName := app.Name
+		appCount := app.Instances
 
 		if running && app.State != "STARTED" {
 			continue // Don't add apps that aren't running when passed -r/--running
@@ -109,43 +123,47 @@ func appsInFoundation(apps []cfclient.App, running bool) Foundation {
 			space = Space{}
 		}
 		// Append this app to the org/space, which works even if there are no existing apps
-		org[spaceName] = append(space, appName)
+		org[spaceName] = append(space, appName+" "+strconv.Itoa(appCount))
 	}
 
 	return foundation
 }
 
-func outputApps(foundation Foundation, jsonOut bool) {
+func outputApps(foundation Foundation, jsonOut bool) string {
+	//	fmt.Printf("%+v\n\n", foundation)
 	if jsonOut {
 		f, err := json.Marshal(&foundation)
 		handleErr("Couldn't convert orgs, spaces, and apps to JSON. Something bad happened.", err)
 
-		fmt.Println(string(f))
+		return string(f)
 
 	} else {
 		f, err := yaml.Marshal(&foundation)
 		handleErr("Couldn't convert orgs, spaces, and apps to YAML. Something bad happened.", err)
 
-		fmt.Println(string(f))
+		return string(f)
 
 	}
 }
 
-func readCfConfig(configFile io.Reader) CfConfig {
+func readCfConfig(configFile io.Reader) (CfConfig, error) {
 	cfConfig := CfConfig{}
 
 	jsonParser := json.NewDecoder(configFile)
 	err := jsonParser.Decode(&cfConfig)
-	handleErr("Unable to parse your cloudfoundry config file. Try deleting it and re-logging in via the cf cli.", err)
 
+	return cfConfig, err
+}
+
+func validateCfConfig(cfConfig CfConfig) error {
 	if cfConfig.Api == "" {
-		fmt.Printf("You are not currently targeting a CloundFoundry instance. Fix that.\n")
-		os.Exit(1)
+		return errors.New("You are not currently targeting a CloundFoundry instance")
 	}
 
 	if cfConfig.Token == "" {
-		fmt.Printf("You are not currently logged in via the cf api. Fix that.\n")
-		os.Exit(1)
+		return errors.New("You are not currently logged in via the cf api")
 	}
-	return cfConfig
+
+	return nil
+
 }
